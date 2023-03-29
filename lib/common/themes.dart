6ad611757f117e3b376d8e50b42d657788e26d02
Muramsyah <ramdhanjr690@gmail.com>
2023-03-29 11:34:24 +0700

    core: add more depedencies and app theme

---
+++ lib/common/themes.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme appTextTheme = TextTheme(
+  displayLarge: GoogleFonts.poppins(
+      fontSize: 93, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  displayMedium: GoogleFonts.poppins(
+      fontSize: 58, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  displaySmall: GoogleFonts.poppins(fontSize: 47, fontWeight: FontWeight.w400),
+  headlineMedium: GoogleFonts.poppins(
+      fontSize: 33, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headlineSmall: GoogleFonts.poppins(fontSize: 23, fontWeight: FontWeight.w400),
+  titleLarge: GoogleFonts.poppins(
+      fontSize: 19, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  titleMedium: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  titleSmall: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyLarge: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyMedium: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  labelLarge: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  bodySmall: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  labelSmall: GoogleFonts.poppins(
+      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
+
+Color appLightPrimaryColor = const Color(0xff672CBC);
+Color appLightSecondaryColor = const Color(0xff9055FF);
+Color appLightPurpleColor = const Color(0xffDF98FA);
+Color appLightOrangeColor = const Color(0xffF9B091);
+Color appLightYellowColor = const Color(0xffFFD08A);
