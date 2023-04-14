11b9f7251df10c565a66da52dd794972332d5bec
Muramsyah <ramdhanjr690@gmail.com>
2023-04-14 13:43:11 +0700

    fix: solve scroll to index error in surah detail page

--- lib/presentation/ui/surah_detail_page.dart
+++ lib/presentation/ui/surah_detail_page.dart
@@ -25,6 +25,7 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
   final ItemPositionsListener itemPositionsListener =
       ItemPositionsListener.create();
   late Surah surah;
+  final ScrollController scrollController = ScrollController();
 
   @override
   void initState() {
@@ -61,20 +62,22 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
             } else if (state is SurahDetailLoaded) {
               surahDetail = state.surahDetail;
               ayah = surahDetail.surahAyah;
-              return ListView(
-                padding: const EdgeInsets.all(0),
-                children: [
-                  _buildSurahBanner(context, surahDetail, textTheme),
-                  SizedBox(height: 24.h),
-                  ScrollablePositionedList.builder(
-                    shrinkWrap: true,
-                    itemCount: surahDetail.totalAyah,
-                    itemBuilder: (context, index) =>
-                        AyahItem(ayah: ayah[index]),
-                    itemScrollController: itemScrollController,
-                    itemPositionsListener: itemPositionsListener,
-                  ),
-                ],
+              return ScrollablePositionedList.builder(
+                shrinkWrap: true,
+                itemCount: surahDetail.totalAyah + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Column(
+                      children: [
+                        _buildSurahBanner(context, surahDetail, textTheme),
+                        const SizedBox(height: 24),
+                      ],
+                    );
+                  }
+                  return AyahItem(ayah: ayah[index - 1]);
+                },
+                itemScrollController: itemScrollController,
+                itemPositionsListener: itemPositionsListener,
               );
             } else {
               return const Center(
@@ -96,6 +99,7 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
       alignment: Alignment.center,
       children: [
         Container(
+          padding: EdgeInsets.symmetric(vertical: 24.h),
           width: MediaQuery.of(context).size.width * .9,
           height: MediaQuery.of(context).size.height * .35,
           decoration: BoxDecoration(
@@ -166,126 +170,6 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
     );
   }
 
-  _buildInfoSurahDialog(BuildContext context, TextTheme textTheme) async {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return BlocBuilder<SurahDetailBloc, SurahDetailState>(
-            builder: (context, state) {
-              if (state is SurahDetailLoading) {
-                return const Dialog(
-                  child: SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  ),
-                );
-              } else if (state is SurahDetailError) {
-                return Dialog(
-                  child: SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Center(
-                      child: Text(state.message),
-                    ),
-                  ),
-                );
-              } else if (state is SurahDetailLoaded) {
-                final surahDetail = state.surahDetail;
-                return Dialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * .35,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width * .9,
-                          height: MediaQuery.of(context).size.height * .35,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                appLightPurpleColor,
-                                appLightPrimaryColor,
-                              ],
-                            ),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Color(0x19000000),
-                                blurRadius: 24,
-                                offset: Offset(0, 20),
-                              ),
-                            ],
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: 30,
-                          child: Opacity(
-                            opacity: .1,
-                            child: SvgPicture.asset(
-                              'assets/quran_item.svg',
-                              width: 300,
-                            ),
-                          ),
-                        ),
-                        Center(
-                          child: Column(
-                            children: [
-                              Text(
-                                surahDetail.latinName,
-                                style: textTheme.headlineMedium!.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                surahDetail.means,
-                                style: textTheme.headlineSmall!
-                                    .copyWith(color: Colors.white),
-                              ),
-                              const SizedBox(height: 12),
-                              const SizedBox(
-                                width: 250,
-                                child: Divider(
-                                  height: 4,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 12),
-                              Text(
-                                '${surahDetail.type} : ${surahDetail.totalAyah}',
-                                style: textTheme.titleMedium!
-                                    .copyWith(color: Colors.white),
-                              ),
-                              const SizedBox(height: 32),
-                              SvgPicture.asset(
-                                'assets/basmallah_item.svg',
-                                width: 260,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              } else {
-                return const Center(
-                  child: Text('Oops something went wrong, try again later..'),
-                );
-              }
-            },
-          );
-        });
-  }
-
   SliverAppBar _buildSliverAppBar(
     TextTheme textTheme,
     BuildContext context,
@@ -319,16 +203,6 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
           },
           icon: const Icon(
             Icons.search,
-            color: Colors.grey,
-            size: 30,
-          ),
-        ),
-        IconButton(
-          onPressed: () {
-            _buildInfoSurahDialog(context, textTheme);
-          },
-          icon: const Icon(
-            Icons.info,
             color: Colors.grey,
             size: 30,
           ),
@@ -375,13 +249,12 @@ class _SurahDetailPageState extends State<SurahDetailPage> {
                     ),
                     textAlign: TextAlign.center,
                     onFieldSubmitted: (value) {
-                      setState(() {
-                        itemScrollController.scrollTo(
-                          index: int.parse(value) - 1,
-                          duration: const Duration(seconds: 2),
-                        );
-                        Navigator.pop(context);
-                      });
+                      Navigator.pop(context);
+                      // scrollController.jumpTo(int.parse(value) - 1);
+                      itemScrollController.scrollTo(
+                        index: int.parse(value),
+                        duration: const Duration(seconds: 2),
+                      );
                     },
                   ),
                 )
