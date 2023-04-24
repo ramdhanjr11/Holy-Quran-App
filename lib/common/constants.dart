2afcf23cf6e04ea8f7ef4951b5652599b5fd2910
Muramsyah <ramdhanjr690@gmail.com>
2023-04-24 13:56:22 +0700

    core: add constant for base url of dua

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -4,5 +4,6 @@ const quranBaseUrl = 'https://equran.id/api/v2/surat';
 const sholatTimeBaseUrl = 'https://api.myquran.com/v1/sholat/jadwal/1226';
 const newsBaseUrl =
     'https://artikel-islam.netlify.app/.netlify/functions/api/rum';
+const duaBaseUrl = 'https://doa-doa-api-ahmadramadhan.fly.dev/api/';
 
 final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
